f8e997d38a27893e8ba807ac0d6462017731a549
user <user@DESKTOP-JBKLJS2>
2024-05-31 17:43:15 +0900

    2024.05.31

--- carcarocean/src/main/java/kr/car/dao/CarDao.java
+++ carcarocean/src/main/java/kr/car/dao/CarDao.java
@@ -299,4 +299,48 @@ public class CarDao {
 			DBUtil.executeClose(rs, pstmt2, conn);
 		}
 	}
+	
+	public void updateCar(CarVO car) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		int cnt = 0;
+		try {
+			conn = DBUtil.getConnection();
+			sql = "UPDATE CAR SET car_maker=?, car_name=?, car_size=?, car_birth=?,"
+					+ " car_cnumber=?,car_fuel_type=?, car_mile=?, car_price=?, car_color=?, car_photo=?, car_auto=?,"
+					+ "car_fuel_efficiency=?, car_use=?, car_cc=?, car_accident=?, car_owner_change=?,"
+					+ "car_design_op=?, car_con_op=?, car_drive_op=?, checker_num=?, car_checker_opinion=? where car_num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(++cnt, car.getCar_maker());
+			pstmt.setString(++cnt, car.getCar_name());
+			pstmt.setInt(++cnt, car.getCar_size());
+			pstmt.setString(++cnt, car.getCar_birth());
+			pstmt.setString(++cnt, car.getCar_cnumber());
+			pstmt.setInt(++cnt, car.getCar_fuel_type());
+			pstmt.setInt(++cnt, car.getCar_mile());
+			pstmt.setInt(++cnt, car.getCar_price());
+			pstmt.setString(++cnt, car.getCar_color());
+			
+			pstmt.setString(++cnt, car.getCar_photo());
+			
+			pstmt.setInt(++cnt, car.getCar_auto());
+			pstmt.setFloat(++cnt, car.getCar_fuel_efficiency());
+			pstmt.setInt(++cnt, car.getCar_use());
+			pstmt.setInt(++cnt, car.getCar_cc());
+			pstmt.setString(++cnt, car.getCar_accident());
+			pstmt.setInt(++cnt, car.getCar_owner_change());
+			pstmt.setString(++cnt, car.getCar_design_op());
+			pstmt.setString(++cnt, car.getCar_con_op());
+			pstmt.setString(++cnt, car.getCar_drive_op());
+			pstmt.setInt(++cnt, car.getChecker_num());
+			pstmt.setString(++cnt, car.getCar_checker_opinion());
+			pstmt.setInt(++cnt, car.getCar_num());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			throw new Exception(e);
+		} finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 }
