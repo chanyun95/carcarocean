bca428e3fddba71c3ffe5a95ed1d0ff053921463
김승연 <tmddus3002@gmail.com>
2024-06-08 19:39:43 +0900

    2024.06.08 19:39 commit

--- carcarocean/src/main/java/kr/item/action/UpdateItemStatusAction.java
+++ carcarocean/src/main/java/kr/item/action/UpdateItemStatusAction.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import kr.controller.Action;
 import kr.item.dao.ItemDao;
 import kr.item.vo.ItemVo;
+import kr.util.FileUtil;
 import kr.util.StringUtil;
 
 public class UpdateItemStatusAction implements Action{
@@ -24,6 +25,13 @@ public class UpdateItemStatusAction implements Action{
 		item.setItem_detail(StringUtil.useBrHtml(item.getItem_detail()));
 		if(item.getMember().getMem_num()!=user_num) {
 			return "/WEB-INF/views/common/warningPage.jsp";
+		}
+		
+		if(item.getItem_photo()!=null) {
+			String[] fileArr = item.getItem_photo().split(",");
+			for(String fileName:fileArr) {
+				FileUtil.removeFile(request, fileName);
+			}
 		}
 		
 		itemDao.updateItemStatus(item_num, 1);
